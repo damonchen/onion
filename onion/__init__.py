a197f3deb852fd34e57f347d6bbff5c1f926af63
ubunoon <netubu@gmail.com>
2013-07-06 12:48:59 +0800

    添加基础信息

---
+++ onion/__init__.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+
+__author__ = 'damon'
+__author_email__ = 'netubu@gmail.com'
+__url__ = 'http://git.oschina.net/damon/onion'
+__license__ = 'BSD'
+version = __version__ = '0.0.1a'
+
+class OnionError(Exception): pass
+
